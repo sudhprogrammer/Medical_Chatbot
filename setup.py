d9b70be0e65736251aa5d7f0af8036c979b748f2
sudhprogrammer <sudhanshugt17@gmail.com>
2025-11-15 06:05:47 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Sudhanshu Kumar",
+    author_email="sudhprogrammer@gmail.com",
+    package=find_packages(),
+    install_requires=[]
+)
